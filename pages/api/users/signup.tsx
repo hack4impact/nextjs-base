caaa600bfc2dc73db292000c8c4c67a2cf421c25
Subha Das <rohan6056@hotmail.com>
2021-04-07 22:01:38 -0700

    Added backend validation for email and password

--- pages/api/users/signup.tsx
+++ pages/api/users/signup.tsx
@@ -3,7 +3,7 @@ import UserController from '../../../backend/controllers/UserController';
 import route from '../../../backend/middleware';
 
 
-route.post(async (req, res) => { new UserController(req, res).signup()})
+route.post(async (req, res) => { await new UserController(req, res).signup()})
 
 
 export default route;
